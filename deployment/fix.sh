b711b82a3ea4cbeccb63234d436b0612fb3410c5
Aaron LaBeau <alabeau@gmail.com>
2021-05-18 17:39:31 -0400

    try to see if sed will update application properties

--- deployment/fix.sh
+++ deployment/fix.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
 sed -i "s|127\.0\.0\.1|$1|" src/main/java/org/couchbase/quickstart/configs/ConfigDb.java
+
+sed -i "s|127\.0\.0\.1|$1|" src/main/java/resources/application.properties
+
 cat src/main/java/org/couchbase/quickstart/configs/ConfigDb.java
+
+cat src/main/resources/application.properties
