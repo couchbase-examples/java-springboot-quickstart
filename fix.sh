de31683a22dbcfdfafc1a91b5f2cc8ab2e2778af
Aaron LaBeau <alabeau@gmail.com>
2021-05-18 16:00:43 -0400

    added running the shell script instead

--- fix.sh
+++ fix.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo $1
-gsed -i "s|127\.0\.0\.1|$1|" src/main/java/org/couchbase/quickstart/configs/ConfigDb.java
+sed -i "s|127\.0\.0\.1|$1|" src/main/java/org/couchbase/quickstart/configs/ConfigDb.java
 cat src/main/java/org/couchbase/quickstart/configs/ConfigDb.java
