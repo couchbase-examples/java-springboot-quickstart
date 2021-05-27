1288f10b09cc6ef1b8566b6b77b09aad46ccd3b0
Denis Rosa <denisrosa@MacBook-Pro.local>
2021-05-27 20:53:19 +0200

    Fixing tests and code

--- src/main/java/org/couchbase/quickstart/configs/WebConfiguration.java
+++ src/main/java/org/couchbase/quickstart/configs/WebConfiguration.java
@@ -7,6 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addRedirectViewController("/", "swagger-ui/");
+//    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "swagger-ui/");
