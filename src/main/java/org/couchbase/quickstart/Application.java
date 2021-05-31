e56ab1df2e045a83b99c957c3a17c6d2c01605cd
Denis Rosa <denisrosa@MacBook-Pro.local>
2021-05-31 15:02:30 +0200

    fixes

--- src/main/java/org/couchbase/quickstart/Application.java
+++ src/main/java/org/couchbase/quickstart/Application.java
@@ -3,7 +3,6 @@ package org.couchbase.quickstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class, proxyBeanMethods = false)
 public class Application {
--- src/main/java/org/couchbase/quickstart/controllers/IndexController.java
+++ src/main/java/org/couchbase/quickstart/controllers/IndexController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
-@Controller
-
+//@Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public ModelAndView index() {
-        return new ModelAndView("redirect:/swagger-ui/");
-    }
+//    @RequestMapping("/")
+//    public ModelAndView index() {
+//        return new ModelAndView("index.html");
+//    }
 }
